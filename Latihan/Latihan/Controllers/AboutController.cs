9c566fdb2624876775e8c89b3a823193a0193cbb
heryana <heryanakasih@gmail.com>
2020-06-19 14:12:04 +0700

    fisrt commit

---
+++ Latihan/Latihan/Controllers/AboutController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Latihan.Models;
+using System.Net;
+using System.Net.Mail; 
+
+namespace Latihan.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult About()
+        {
+            List<Person> person = new List<Person>
+            {
+                new Person { Name = "Iqbal", Kota = "Depok" },
+                new Person { Name = "Brandon", Kota = "Bogor" },
+                new Person { Name = "Julian", Kota = "Tangerang" }
+            };
+            return View(person);
+        }
+
+        public ActionResult ProcessRequest() 
+        {
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            //Or your Smtp Email ID and Password
+            smtp.EnableSsl = true;
+            return View();
+        }
+        //send mail
+    }
+}
---
+++ Latihan/Latihan/Controllers/PortfolioController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Latihan.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        //
+        // GET: /Portfolio/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+    }
+}
