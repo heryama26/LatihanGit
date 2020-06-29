fba951f12476b249245c6c5ad1d457ca6a72ca7b
heryana <heryanakasih@gmail.com>
2020-06-29 11:19:07 +0700

    login

--- Latihan/Latihan/Controllers/HomeController.cs
+++ Latihan/Latihan/Controllers/HomeController.cs
@@ -18,35 +18,5 @@ namespace Latihan.Controllers
             return View();
         }
 
-        public ActionResult Login()
-        {
-            SqlConnection conn = new SqlConnection();
-            conn.ConnectionString =
-            "Data Source=DESKTOP-B66Q0OG\\MSSQLSERVER2016;" +
-            "Initial Catalog=LATIHAN_MAVEN;" +
-            "Integrated Security=SSPI;";
-            conn.Open();
-
-            string sql = "SELECT*from UserProfile where";
-            SqlCommand cmd = new SqlCommand(sql, conn);
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            List<UserProfile> listDataUser = new List<UserProfile>();
-            while (reader.Read())
-            {
-                UserProfile user = new UserProfile
-                {
-                   USERNAME = Convert.ToString(reader["UserName"]),
-                   PASSWORD = Convert.ToString(reader["Password"])
-                };
-                listDataUser.Add(user);
-            }
-            reader.Close();
-            conn.Close();
-
-            ViewData["ListDataUser"] = listDataUser;
-
-            return View();
-        }
     }
 }
---
+++ Latihan/Latihan/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Latihan.Models;
+using System.Data.SqlClient;
+
+namespace Latihan.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+            "Data Source=DESKTOP-B66Q0OG\\MSSQLSERVER2016;" +
+            "Initial Catalog=LATIHAN_MAVEN;" +
+            "Integrated Security=SSPI;";
+            conn.Open();
+
+            string sql = "SELECT*from UserProfile";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<UserProfile> listDataUser = new List<UserProfile>();
+            while (reader.Read())
+            {
+                UserProfile user = new UserProfile
+                {
+                    USERNAME = Convert.ToString(reader["UserName"]),
+                    PASSWORD = Convert.ToString(reader["Password"])
+                };
+                listDataUser.Add(user);
+            }
+            reader.Close();
+            conn.Close();
+
+            ViewData["ListDataUser"] = listDataUser;
+
+            return View();
+        }
+    }
+}
